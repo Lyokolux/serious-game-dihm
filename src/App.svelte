173ffdf1152d708600a6529eaf09f1bb25fcd441
johannchopin <johannchopin@pm.me>
2022-03-09 17:59:52 +0100

    add app title

--- src/App.svelte
+++ src/App.svelte
@@ -17,6 +17,7 @@
 </script>
 
 <svelte:head>
+  <title>Lost in nature</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </svelte:head>
 
